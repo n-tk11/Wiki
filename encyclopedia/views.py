a5a1302c215b5fefa2a44af78a5a9d69709f4406
tul <tulsorawit@hotmail.com>
2021-06-17 14:54:49 +0700

    Index now can link to entry pages

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,22 @@
+from django.http.response import HttpResponse,HttpResponseNotFound
 from django.shortcuts import render
+from django.http import Http404
 
 from . import util
+from markdown2 import Markdown
 
+markdowner = Markdown()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    if title in util.list_entries():
+        return render(request, "encyclopedia/entry.html", {
+            "content": markdowner.convert(util.get_entry(title)),
+            "title": title
+        })
+    else:
+        return HttpResponseNotFound('<h1>404 Page not found</h1>')
