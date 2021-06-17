76d6c329003966fd94a4de0aa05e3f5eb29a7441
tul <tulsorawit@hotmail.com>
2021-06-17 23:28:47 +0700

    Search now can search with substring

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -5,12 +5,13 @@ from . import util
 from markdown2 import Markdown
 from django import forms
 from django.urls import reverse
+import re
 
 markdowner = Markdown()
 
 
 class Search(forms.Form):
-    stitle = forms.CharField(widget=forms.TextInput(attrs={'class' : 'search', 'placeholder': 'Search'}))
+    stitle = forms.CharField(label="",widget=forms.TextInput(attrs={'class' : 'search', 'placeholder': 'Search'}))
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -19,17 +20,17 @@ def index(request):
     })
 
 def entry(request, title):
-    if title in util.list_entries():
-        return render(request, "encyclopedia/entry.html", {
-            "content": markdowner.convert(util.get_entry(title)),
-            "title": title
-        })
-    else:
-        return HttpResponseNotFound('<h1>404 Page not found</h1>')
+    return render(request, "encyclopedia/entry.html", {
+        "content": markdowner.convert(util.get_entry(title)),
+        "title": title
+    })
 
 def search(request):
     form = Search(request.POST)
 
     if form.is_valid():
         title = form.cleaned_data["stitle"]
-        return HttpResponseRedirect(reverse("encyclopedia:entry",args=[title]))
+        for entry in util.list_entries():
+            if entry.lower().find(title) >= 0:
+                return HttpResponseRedirect(reverse("encyclopedia:entry",args=[entry]))
+        return HttpResponseNotFound('<h1>404 Page not found</h1>')            
