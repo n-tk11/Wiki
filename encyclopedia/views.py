92313deb65f0e31e9c0f7d0f0202a97e9765882c
tul <tulsorawit@hotmail.com>
2021-06-17 15:10:51 +0700

    Index now can link to entry pages

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,11 +1,13 @@
+from logging import PlaceHolder
 from django.http.response import HttpResponse,HttpResponseNotFound
 from django.shortcuts import render
 from django.http import Http404
-
 from . import util
 from markdown2 import Markdown
+from django import forms
 
 markdowner = Markdown()
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
